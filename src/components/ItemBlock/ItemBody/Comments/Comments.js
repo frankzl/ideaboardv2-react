848d82d6c797879b0f7984d5b654c23bdda615bf
Frank Lu <frankz.lu@googlemail.com>
2017-12-06 15:00:00 +0100

    prepared comment feat

--- src/components/ItemBlock/ItemBody/Comments/Comments.js
+++ src/components/ItemBlock/ItemBody/Comments/Comments.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import classes from '../ItemBody.css'
+import localClasses from './Comments.css'
+import { Comment } from "../../../../models/IdeaBody/CommentsModel";
 
-const Comments = (props) => <div className={classes.itemWrapper}>Comments</div>
+const Comments = ( props ) => {
+
+    comments = [
+        new Comment('Frank', 'fdsakjlf dfa d fasdfjdasl', new Date(), 5),
+        new Comment('Frank', 'fdsakjlf dfa d fasdfjdasl', new Date(), 5),
+        new Comment('Frank', 'fdsakjlf dfa d fasdfjdasl', new Date(), 5)
+    ]
+
+    return (
+        <div style={{flexDirection: 'column'}} className = { classes.itemWrapper + ' ' +localClasses.commentsWrapper }>
+            <div>Comments</div>
+            <hr className={localClasses.hr}/>
+            {comments.map(
+                comment => {
+                    return null
+                }
+            )}
+        </div>
+    )
+}
 
 export default Comments
--- src/components/ItemBlock/ItemBody/Survey/Survey.js
+++ src/components/ItemBlock/ItemBody/Survey/Survey.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Checkbox from 'material-ui/Checkbox'
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Check from 'material-ui/svg-icons/navigation/check';
 
 import classes from '../ItemBody.css'
 
 
-const Survey = (props) => {
-    return <div className={classes.itemWrapper}>
-        {props.body.options.map(
+const Survey = ( props ) => {
+    return <div className = { classes.itemWrapper }>
+        { props.body.options.map(
             option => {
                 return <Checkbox
-                    label={option.name} />
+                    label = { option.name } />
             }
-        )}
-        </div>
+        ) }
+        <FloatingActionButton mini={true}>
+            <Check/>
+        </FloatingActionButton>
+    </div>
 }
 
 export default Survey
